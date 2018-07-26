ea27c407d2a4c833d51742a3ff48e2e54d7fedc7
psalinas <psalinas@mobid.cl>
2018-07-26 17:05:12 -0400

    form

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,53 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
-import { Usuario, AuthService } from '../../services/auth.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Usuario, AuthService } from "../../services/auth.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styles: [`
-  .ng-invalid.ng-touched:not(form) {
-    border: 1px solid red;
-  }
-  `]
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styles: [
+    `
+      .ng-invalid.ng-touched:not(form) {
+        border: 1px solid red;
+      }
+    `
+  ]
 })
 export class SignupComponent implements OnInit {
-  
-forma: FormGroup;
+  forma: FormGroup;
 
-paises = [{
-  codigo: "CRI",
-  nombre: "Costa Rica"
-},
-{
-  codigo: "ESP",
-  nombre: "España"
-}
-];
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: "pablo",
+      apellido: "salinas"
+    },
+    correo: "pablo@gmial.com"
+  };
 
-  constructor( authService: AuthService) {
+  paises = [
+    {
+      codigo: "CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo: "ESP",
+      nombre: "España"
+    }
+  ];
 
-    this.forma = new FormGroup ({
-      'nombre': new FormControl ('', [Validators.required, Validators.minLength(3)] ),
-      'apellido': new FormControl ('', [Validators.required] ),
-      'correo': new FormControl ('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+  constructor(authService: AuthService) {
+
+    console.log(this.usuario);
+    
+    this.forma = new FormGroup({
+      nombre: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      apellido: new FormControl("", [Validators.required]),
+      correo: new FormControl("", [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$")
+      ])
     });
-   }
-
-  ngOnInit() {
   }
-  
+
+  ngOnInit() {}
 
   guardar() {
     // console.log("agregar usuario");
     console.log("NgForm ", this.forma);
     // console.log("valor", forma.value);
     // console.log("Usuario", this.forma);
-    
   }
-
- 
 }
-
-
